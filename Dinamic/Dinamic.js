1c3bd5c561153133fd3a3872c898828e35bf461e
nadezdatsygankova <nadia_pritchina@mail.ru>
2020-07-22 16:31:44 -0400

    Dinamic

---
+++ Dinamic/Dinamic.js
@@ -0,0 +1,26 @@
+onst images = [
+    {
+        name: "image1.jpg",
+        description: "image1 description.",
+        dimensions: {
+            width: 638,
+		height: 803
+        }
+    },
+    {
+        name: "image2.jpg",
+        description: "image2 description.",
+        dimensions: {
+            width: 622,
+		height: 405
+        }
+    },
+    {
+        name: "image3.jpg",
+        description: "image3 description.",
+        dimensions: {
+            width: 579,
+		height: 862
+        }
+    }
+];
