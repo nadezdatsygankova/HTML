5a8bc87b3f93012711e42b6a911d5ec680ceedb5
nadezdatsygankova <nadia_pritchina@mail.ru>
2020-07-23 02:41:08 -0400

    new version

--- Dinamic/Dinamic.js
+++ Dinamic/Dinamic.js
@@ -1,26 +1,144 @@
-onst images = [
+/*const images = [
     {
-        name: "image1.jpg",
-        description: "image1 description.",
+        name: "img/tiger.jpg",
+        description: "Tiger.",
         dimensions: {
-            width: 638,
-		height: 803
+            width: 442,
+		height: 640
         }
     },
     {
-        name: "image2.jpg",
-        description: "image2 description.",
+        name: "img/bear.jpg",
+        description: "Polar bear",
         dimensions: {
-            width: 622,
-		height: 405
+            width: 425,
+		height: 600
         }
     },
     {
-        name: "image3.jpg",
-        description: "image3 description.",
+        name: "img/camel.jpg",
+        description: "Camel.",
         dimensions: {
-            width: 579,
-		height: 862
+            width: 360,
+		height: 640
         }
-    }
+    },
+
+    {
+        name: "img/capybara.jpg",
+        description: "Capybara.",
+        dimensions: {
+            width: 480,
+		height: 640
+        }
+
+    },
+    {
+        name: "img/dear.jpg",
+        description: "Dear.",
+        dimensions: {
+            width: 360,
+		height: 640
+        }
+
+    },
+    {
+        name: "img/donkey.jpg",
+        description: "Donkey.",
+        dimensions: {
+            width: 360,
+		height: 640
+        }
+
+    },
+    {
+        name: "img/monkey1.jpg",
+        description: "Monkey.",
+        dimensions: {
+            width: 365,
+		height: 550
+        }
+
+    },
+    {
+        name: "img/monkey2.jpg",
+        description: "Serious monkey.",
+        dimensions: {
+            width: 425,
+		height: 640
+        }
+
+    },
+    {
+        name: "img/ostrich.jpg",
+        description: "Ostrich.",
+        dimensions: {
+            width: 480,
+		height: 640
+        }
+
+    },
+
+
+
 ];
+*/
+
+var name=["img/tiger.jpg", "img/bear.jpg"];
+var description=["Tiger.","Polar bear"]
+var intArray=new Array();
+var i=0;
+
+
+
+
+
+/*
+function fonload()
+{
+   var expandImg= document.getElementById("lblResult").src;
+   var firstImg = document.getElementById("first").src;
+   expandImg=firstImg;
+};
+// picture above image
+/*
+(images[0].description)//desription
+(images[1].description)
+
+/*
+function fnOnload () {
+=${document.getElementById("")}
+*/
+
+
+/*
+
+function fInitialize()
+    {
+		lblResult.innerHTML = "";
+    };
+*/
+    function myFunction(images) {
+        var expandImg = document.getElementById("lblResult");
+      
+       /* expandImg.src = images.src;*/
+
+        expandImg.src = name[parseInt(images)];
+        /*
+        expandImg.innerHTML = name[parseInt(images.name)];
+        
+        /*expandImg.parentElement.style.display = "block";*/
+        /*expandImg.style.cssText=" width: 400; height: 700 "*/
+    };
+    
+
+/*
+//функция для изменения изображения
+function fCSSa(images)
+	{
+       var findCon= document.getElementById("lblResult");
+      
+        findCon.src = images.src;
+        findCon.style.cssText="width: 400; height: 700";
+        //document.getElementById("camel").style.cssText="filter:brightness(200%)";
+	}*/
