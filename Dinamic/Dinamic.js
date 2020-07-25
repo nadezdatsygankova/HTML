f9b36671c5820fa29c1562257a3cc0a58d22a04a
nadezdatsygankova <nadia_pritchina@mail.ru>
2020-07-25 19:08:35 -0400

    Finall

--- Dinamic/Dinamic.js
+++ Dinamic/Dinamic.js
@@ -130,22 +130,7 @@ function fonload()
        var heightBoxS=contImg.style.height;
        var heightBox=parseInt(heightBoxS.match(/\d+/));
        console.log(heightBox);
-/*
 
-       var resultWeght=weghtImg/widthBox;
-        console.log(resultWeght);
-       var widthN=weghtImg/resultWeght; //finish result for img -400
-       console.log(widthN);
-
-       var resultHeght=heghtImg/heightBox;
-       var heightN=heghtImg/resultHeght; 
-       console.log(heightN);
-       expandImg.height=heightN;
-       expandImg.width=widthN;
-       console.log(expandImg.height);
-       console.log(expandImg.width);
-
-*/
       var kof=Number(heghtImg/weghtImg);//koff 
        console.log(kof);
 
@@ -155,7 +140,6 @@ function fonload()
         weghtImg=(weghtImg/kof);
         console.log(weghtImg);
 
-       // expandImg.style.height=heghtImg+"px";
        expandImg.style.height="auto";
         expandImg.style.width=weghtImg+"px";
        }
@@ -163,8 +147,7 @@ function fonload()
        {
         heghtImg=heightBox;
         console.log(heghtImg);
-       // weghtImg=(weghtImg/kof);
-      //  console.log(weghtImg);
+
         expandImg.style.height=heghtImg+"px";
         expandImg.style.width="auto";
        }
@@ -184,70 +167,6 @@ function fonload()
 
 
 
-      //стрелки
-/*function key(event)
-{
-    
-    
-    evt = (event) ? event : window.event;							//	window.event is MS IE specific
-    var code = (event.which) ? event.which : event.keyCode;
-    console.log(code);
-
-
-
-    var expandImgO = document.getElementById("textCoverImg");
-    var i =Number(expandImgO.getAttribute('value'));
-    console.log(i);
-
-    var len=Number(images.length);
-    console.log(len);
-    
-    if( code == 38 ) { //  up
-        if((i<=len) && (i>0))
-        {
-            console.log(i);
-            myFunction((i-1));
-        }
-        else if (i<=0)
-        {
-            myFunction(len);
-        }
-        
-    }
-    if( code == 40 ) { //  down
-        if(i<=Number(len) && i>0)
-        {
-            myFunction((i+1));
-        }
-        else if(i>=Number(len))
-        {
-            myFunction(0);
-        }
-    }
-
-
-};
-*//*
-      document.body.addEventListener( "keydown", function( e ) {
-        var code = e.keyCode;
-        var evt = new MouseEvent( "click" );  // нажатие мыши
-                 
-       
-                    
-    }, false); */  
-
-
- /*   function fOnkeyDown(evt)
-    {
-   
-		var charCode = (evt.which) ? evt.which : evt.keyCode;
-	
-		return charCode;
-    }	
-
-
-
-*/
 
 
     document.onkeydown=function(event){
@@ -273,19 +192,19 @@ function fonload()
     {
         var resultBox= document.getElementById("lblResult").getAttribute("src");
        // console.log(resultBox);
-      //  var currentImageNumber=resultBox.substring(resultBox.lastIndexOf('/')+1,resultBox.lastIndexOf('/')+2 );
+    
       var currentImageNumber=parseInt(resultBox.match(/\d+/));
       //console.log(currentImageNumber);
       var numCurr=Number(currentImageNumber); //convert to number
        if(numCurr < (images.length-1))
        {
         myFunction(Number(numCurr+1));
-     //   resultBox.setAttribute("src","/img" +(numCurr+1)+".jpg");
+  
        }
         else if(numCurr=(images.length-1))
         {
             myFunction(0);
-            //  resultBox.setAttribute("src","/img" +1+".jpg");
+  
         }
     };
 
@@ -294,119 +213,28 @@ function fonload()
     {
         var resultBox= document.getElementById("lblResult").getAttribute("src");
         console.log(resultBox);
-      //  var currentImageNumber=resultBox.substring(resultBox.lastIndexOf('/')+1,resultBox.lastIndexOf('/')+2 );
+ 
       var currentImageNumber=parseInt(resultBox.match(/\d+/));
       console.log(currentImageNumber);
       var numCurr=Number(currentImageNumber); //convert to number
-          //   myFunction(numCurr-2);
-      console.log(numCurr);
+
+    //  console.log(numCurr);
 
       var back=Number(numCurr-2);
-      console.log(back);
+   //   console.log(back);
 
       var firstBack=Number((images.length-1));
-      console.log(firstBack);
+    //  console.log(firstBack);
 
        if(numCurr!=1)
        {
               myFunction(back);
-      //  myFunction(Number(numCurr-1));
-     //   resultBox.setAttribute("src","/img" +(numCurr+1)+".jpg");
+ 
        }
         else if(numCurr==1)
         {
 
             myFunction(firstBack);
-            //  resultBox.setAttribute("src","/img" +1+".jpg");
+
         }
     };
-        
-/*
-    function changeImageBack()
-    {
-        var resultBox= document.getElementById("lblResult").getAttribute("src");
-        console.log(resultBox);
-       // var currentImageNumber=resultBox.substring(resultBox.lastIndexOf('/')+1,resultBox.lastIndexOf('/')+2 );
-       var currentImageNumber=parseInt(resultBox.match(/\d+/));
-       console.log(currentImageNumber);
-       var numCurr=Number(currentImageNumber); //convert to number
-       console.log(images.length);
-
-     //  if((numCurr <= images.length) && numCurr>1)
-     //  {
-
-        myFunction(Number(currentImageNumber-1));
-        //myFunction(Number(numCurr-1));
-      //  console.log(Number(numCurr-1));
-       // resultBox.setAttribute("src","/img" +(numCurr-1)+".jpg");
-     //  }
-      //  else if(numCurr=1)
-     //   {
-     //       myFunction((Number(images.length)-1));
-            // resultBox.setAttribute("src","/img" +images.length+".jpg");
-     //   }
-    };
-/*
-    function myFunction(num) {
-        var expandImg=document.getElementById("lblResult");
-        console.log(expandImg);
-        var expandImgSrc = document.getElementById("lblResult").getAttribute("src");
-      
-        //variable for value
- 
-      var nameImages=images[num].name;
-      expandImgSrc= nameImages; //src of image in the container
-//text
-var textCon=document.getElementById("text");
-    
-textCon.innerText=images[num].description +"\n"+ " width: "  + images[num].dimensions.width + "\n"+ " height: " +images[num].dimensions.height;
-
-var weghtImg=Number(images[num].dimensions.width); //width images original
-// console.log(weghtImg);
-var heghtImg=Number(images[num].dimensions.height); //height images original
-// console.log(heghtImg);
-
-
-
-
-var widthBox=expandImg.width;  //box width
-var heightBox=expandImg.height;//box heght
-
-var resultWeght=weghtImg/widthBox;
-
-var widthN=weghtImg/resultWeght; //finish result for img -400
-
-
-var resultHeght=heghtImg/heightBox;
-var heightN=heghtImg/resultHeght; 
-
-expandImg.height=heightN;
-expandImg.width=widthN;
-
-    };
-//console.log(expandImg.height);
-//console.log(expandImg.width);
-
-//
-
-
-
-
-
-
-/*
-        
-        var imageSrcTiger=document.getElementById('tiger').getAttribute("src");
-        console.log(imageSrcTiger);
-
-           
-
-
-
-        var currentImageNumber=imageSrcTiger.substring(imageSrcTiger.lastIndexOf('/')+1,imageSrcTiger.lastIndexOf('/')+2 );
-        console.log(currentImageNumber);
-       var resultBox= document.getElementById("lblResult");
-       resultBox.setAttribute("src", "/Images" +(Number(currentImageNumber)+1)+".jpg");
-
-
-    }*/
